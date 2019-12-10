eb91ebe26a088dcade4efc335be2b4a23485ef8c
hodory <mo02@outlook.kr>
2019-12-10 21:25:03 +0900

    테스트 코드 리팩토링

--- rest-api/rest/src/test/java/dev/hodory/restapi/events/EventControllerTests.java
+++ rest-api/rest/src/test/java/dev/hodory/restapi/events/EventControllerTests.java
@@ -17,45 +17,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.hodory.restapi.common.RestDocsConfiguration;
+import dev.hodory.restapi.common.BaseControllerTest;
 import dev.hodory.restapi.common.TestDescription;
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTests {
-
-  @Autowired
-  MockMvc mockMvc;
-
-  @Autowired
-  ObjectMapper objectMapper;
+public class EventControllerTests extends BaseControllerTest {
 
   @Autowired
   EventRepository eventRepository;
-
-  @Autowired
-  ModelMapper modelMapper;
 
   @Test
   @TestDescription("정상적인 이벤트를 등록하는 테스트")
--- rest-api/rest/src/test/java/dev/hodory/restapi/index/IndexControllerTests.java
+++ rest-api/rest/src/test/java/dev/hodory/restapi/index/IndexControllerTests.java
@@ -4,28 +4,10 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import dev.hodory.restapi.common.RestDocsConfiguration;
+import dev.hodory.restapi.common.BaseControllerTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class IndexControllerTests {
-
-  @Autowired
-  MockMvc mockMvc;
+public class IndexControllerTests extends BaseControllerTest {
 
   @Test
   public void index() throws Exception {
