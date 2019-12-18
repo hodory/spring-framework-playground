217d9594857669398e9b5eccf5a19f7658e97907
hodory <mo02@outlook.kr>
2019-12-18 22:23:43 +0900

    스프링 시큐리티 폼 인증 설정

--- rest-api/rest/src/main/java/dev/hodory/restapi/configs/AppConfig.java
+++ rest-api/rest/src/main/java/dev/hodory/restapi/configs/AppConfig.java
@@ -1,6 +1,13 @@
 package dev.hodory.restapi.configs;
 
+import dev.hodory.restapi.accounts.Account;
+import dev.hodory.restapi.accounts.AccountRole;
+import dev.hodory.restapi.accounts.AccountService;
+import java.util.Set;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -17,5 +24,23 @@ public class AppConfig {
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
+  }
+
+  @Bean
+  public ApplicationRunner applicationRunner() {
+    return new ApplicationRunner() {
+      @Autowired
+      AccountService accountService;
+
+      @Override
+      public void run(ApplicationArguments args) throws Exception {
+        final Account account = Account.builder()
+            .email("me@hodory.dev")
+            .password("password@test")
+            .roles(Set.of(AccountRole.ADMIN))
+            .build();
+        accountService.saveAccount(account);
+      }
+    };
   }
 }
