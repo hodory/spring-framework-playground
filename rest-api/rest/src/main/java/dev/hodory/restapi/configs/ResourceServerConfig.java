c5b06c5661805c3fb5cae959357eb74abd70d2c2
hodory <mo02@outlook.kr>
2019-12-22 20:01:57 +0900

    API 권한 설정 오류 수정 `anonymous`로 설정하면 인증된 사용자가 접근을 못하는 문제 수정

--- rest-api/rest/src/main/java/dev/hodory/restapi/configs/ResourceServerConfig.java
+++ rest-api/rest/src/main/java/dev/hodory/restapi/configs/ResourceServerConfig.java
@@ -20,7 +20,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
   @Override
   public void configure(HttpSecurity http) throws Exception {
     http.anonymous().and().authorizeRequests()
-        .mvcMatchers(HttpMethod.GET, "/api/**").anonymous()
+        .mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .exceptionHandling()
