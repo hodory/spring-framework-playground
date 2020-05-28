a27efae95e92fcce7313d9a9e7d7f7c7d497f254
hodory <mo02@outlook.kr>
2020-05-28 23:14:04 +0900

    시큐리티 스터디 프로젝트 추가

---
+++ basicsecurity/src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.security.basicsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 인증 처리
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        // 인가 처리
+        http
+                .formLogin();
+    }
+}
